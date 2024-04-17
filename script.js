cee8d63e7d7dd24ff8329484473f06d4ba6e358d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-17 13:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 function convertToRoman(num) {
-    const obj = [
+    const romanSymbols = [
         ['M', 1000],
+        ['CM', 900],
         ['D', 500],
+        ['CD', 400],
         ['C', 100],
+        ['XC', 90],
         ['L', 50],
+        ['XL', 40],
         ['X', 10],
+        ['IX', 9],
         ['V', 5],
+        ['IV', 4],
         ['I', 1]
     ];
 
     let romanNumeral = '';
 
-    for (let i = 0; i < obj.length; i++) {
-        while (num >= obj[i][1]) {
-            romanNumeral += obj[i][0];
-            num -= obj[i][1];
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            romanNumeral += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
         }
     }
 
     return romanNumeral;
 }
 
-console.log(convertToRoman(14));   
-console.log(convertToRoman(798));  
-console.log(convertToRoman(36));   
+console.log(convertToRoman(14)); 
+console.log(convertToRoman(798)); 
+console.log(convertToRoman(36)); 
 
 module.exports = convertToRoman;
